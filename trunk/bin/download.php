da4525b71ca9b235b24ab89ba45be42c245bf67a
EirikJ <info@example.com>
2012-08-16 17:13:15 +0000

    Make use of additional arguments, if provided.
    git-svn-id: http://svn.projects.ez.no/ezcopy@15 5a7bd14c-e87a-49de-aa6e-fb7969b1d886

--- trunk/bin/download.php
+++ trunk/bin/download.php
@@ -10,6 +10,12 @@ $identifier		= $argv[1];
 $remoteFile		= $argv[2];
 $localFile		= $argv[3];
 
+// if additional arguments are provided
+if(isset($argv[4]))
+{
+	$copy->setAdditionalOptions(array_slice($argv, 4));
+}
+
 // get details for account
 $copy->selectAccount($identifier);
 
